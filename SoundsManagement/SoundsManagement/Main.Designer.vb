fcee4e369fda70149768f34d48151aef40845ae0
peteperson2000@gmail.com <peteperson2000@gmail.com@874bb170-e4d2-9f33-9e38-2bebc6fb2eb0>
2011-03-30 16:04:54 +0000

    Third attempt

--- SoundsManagement/SoundsManagement/Main.Designer.vb
+++ SoundsManagement/SoundsManagement/Main.Designer.vb
@@ -22,12 +22,11 @@ Partial Class Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
-        Me.SoundsDataSet = New SoundsManagement.SoundsDataSet()
-        Me.FilesDataTableBindingSource = New System.Windows.Forms.BindingSource()
-        Me.FilesDataTableTableAdapter = New SoundsManagement.SoundsDataSetTableAdapters.FilesDataTableTableAdapter()
-        Me.SoundsDataSetBindingSource = New System.Windows.Forms.BindingSource()
         Me.SoundsGrid = New DevExpress.XtraGrid.GridControl()
+        Me.FilesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.SoundsDataSet = New SoundsManagement.SoundsDataSet()
         Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.colCreator = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colLibrary = New DevExpress.XtraGrid.Columns.GridColumn()
@@ -42,44 +41,45 @@ Partial Class Main
         Me.colRating = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colFilename = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colTags = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.AxWMP = New AxWMPLib.AxWindowsMediaPlayer()
-        CType(Me.SoundsDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.FilesDataTableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.SoundsDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnLibPath = New System.Windows.Forms.Button()
+        Me.wmp = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.btnPlaySound = New System.Windows.Forms.Button()
+        Me.FilesTableAdapter = New SoundsManagement.SoundsDataSetTableAdapters.FilesTableAdapter()
+        Me.FilesJoinedBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.FilesJoinedTableAdapter = New SoundsManagement.SoundsDataSetTableAdapters.FilesJoinedTableAdapter()
         CType(Me.SoundsGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FilesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SoundsDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.AxWMP, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.wmp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FilesJoinedBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'SoundsGrid
+        '
+        Me.SoundsGrid.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SoundsGrid.DataSource = Me.FilesJoinedBindingSource
+        Me.SoundsGrid.Location = New System.Drawing.Point(0, 0)
+        Me.SoundsGrid.MainView = Me.GridView1
+        Me.SoundsGrid.Name = "SoundsGrid"
+        Me.SoundsGrid.Size = New System.Drawing.Size(648, 388)
+        Me.SoundsGrid.TabIndex = 0
+        Me.SoundsGrid.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'FilesBindingSource
+        '
+        Me.FilesBindingSource.DataMember = "Files"
+        Me.FilesBindingSource.DataSource = Me.SoundsDataSet
         '
         'SoundsDataSet
         '
         Me.SoundsDataSet.DataSetName = "SoundsDataSet"
         Me.SoundsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'FilesDataTableBindingSource
-        '
-        Me.FilesDataTableBindingSource.DataMember = "FilesDataTable"
-        Me.FilesDataTableBindingSource.DataSource = Me.SoundsDataSet
-        '
-        'FilesDataTableTableAdapter
-        '
-        Me.FilesDataTableTableAdapter.ClearBeforeFill = True
-        '
-        'SoundsDataSetBindingSource
-        '
-        Me.SoundsDataSetBindingSource.DataSource = Me.SoundsDataSet
-        Me.SoundsDataSetBindingSource.Position = 0
-        '
-        'SoundsGrid
-        '
-        Me.SoundsGrid.DataSource = Me.FilesDataTableBindingSource
-        Me.SoundsGrid.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SoundsGrid.Location = New System.Drawing.Point(0, 0)
-        Me.SoundsGrid.MainView = Me.GridView1
-        Me.SoundsGrid.Name = "SoundsGrid"
-        Me.SoundsGrid.Size = New System.Drawing.Size(648, 435)
-        Me.SoundsGrid.TabIndex = 0
-        Me.SoundsGrid.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
         '
         'GridView1
         '
@@ -91,6 +91,7 @@ Partial Class Main
         '
         Me.colCreator.FieldName = "Creator"
         Me.colCreator.Name = "colCreator"
+        Me.colCreator.OptionsColumn.AllowEdit = False
         Me.colCreator.OptionsColumn.ReadOnly = True
         Me.colCreator.Visible = True
         Me.colCreator.VisibleIndex = 0
@@ -99,6 +100,7 @@ Partial Class Main
         '
         Me.colLibrary.FieldName = "Library"
         Me.colLibrary.Name = "colLibrary"
+        Me.colLibrary.OptionsColumn.AllowEdit = False
         Me.colLibrary.OptionsColumn.ReadOnly = True
         Me.colLibrary.Visible = True
         Me.colLibrary.VisibleIndex = 1
@@ -107,6 +109,7 @@ Partial Class Main
         '
         Me.colCD.FieldName = "CD"
         Me.colCD.Name = "colCD"
+        Me.colCD.OptionsColumn.AllowEdit = False
         Me.colCD.OptionsColumn.ReadOnly = True
         Me.colCD.Visible = True
         Me.colCD.VisibleIndex = 2
@@ -115,6 +118,7 @@ Partial Class Main
         '
         Me.colYear.FieldName = "Year"
         Me.colYear.Name = "colYear"
+        Me.colYear.OptionsColumn.AllowEdit = False
         Me.colYear.OptionsColumn.ReadOnly = True
         Me.colYear.Visible = True
         Me.colYear.VisibleIndex = 3
@@ -123,6 +127,7 @@ Partial Class Main
         '
         Me.colTrack.FieldName = "Track"
         Me.colTrack.Name = "colTrack"
+        Me.colTrack.OptionsColumn.AllowEdit = False
         Me.colTrack.OptionsColumn.ReadOnly = True
         Me.colTrack.Visible = True
         Me.colTrack.VisibleIndex = 4
@@ -131,6 +136,7 @@ Partial Class Main
         '
         Me.colIndex.FieldName = "Index"
         Me.colIndex.Name = "colIndex"
+        Me.colIndex.OptionsColumn.AllowEdit = False
         Me.colIndex.OptionsColumn.ReadOnly = True
         Me.colIndex.Visible = True
         Me.colIndex.VisibleIndex = 5
@@ -139,6 +145,7 @@ Partial Class Main
         '
         Me.colCategory.FieldName = "Category"
         Me.colCategory.Name = "colCategory"
+        Me.colCategory.OptionsColumn.AllowEdit = False
         Me.colCategory.OptionsColumn.ReadOnly = True
         Me.colCategory.Visible = True
         Me.colCategory.VisibleIndex = 6
@@ -148,6 +155,7 @@ Partial Class Main
         Me.colSubCategory.FieldName = "SubCategory"
         Me.colSubCategory.FilterMode = DevExpress.XtraGrid.ColumnFilterMode.DisplayText
         Me.colSubCategory.Name = "colSubCategory"
+        Me.colSubCategory.OptionsColumn.AllowEdit = False
         Me.colSubCategory.OptionsColumn.ReadOnly = True
         Me.colSubCategory.Visible = True
         Me.colSubCategory.VisibleIndex = 7
@@ -156,6 +164,7 @@ Partial Class Main
         '
         Me.colDescription.FieldName = "Description"
         Me.colDescription.Name = "colDescription"
+        Me.colDescription.OptionsColumn.AllowEdit = False
         Me.colDescription.OptionsColumn.ReadOnly = True
         Me.colDescription.OptionsFilter.AllowAutoFilter = False
         Me.colDescription.OptionsFilter.AllowFilterModeChanging = DevExpress.Utils.DefaultBoolean.[True]
@@ -167,66 +176,114 @@ Partial Class Main
         '
         Me.colTime.FieldName = "Time"
         Me.colTime.Name = "colTime"
+        Me.colTime.OptionsColumn.AllowEdit = False
         Me.colTime.OptionsColumn.ReadOnly = True
         Me.colTime.Visible = True
-        Me.colTime.VisibleIndex = 9
+        Me.colTime.VisibleIndex = 12
         '
         'colRating
         '
         Me.colRating.FieldName = "Rating"
         Me.colRating.Name = "colRating"
         Me.colRating.Visible = True
-        Me.colRating.VisibleIndex = 10
+        Me.colRating.VisibleIndex = 9
         '
         'colFilename
         '
         Me.colFilename.FieldName = "Filename"
         Me.colFilename.Name = "colFilename"
+        Me.colFilename.OptionsColumn.AllowEdit = False
         Me.colFilename.OptionsColumn.ReadOnly = True
         Me.colFilename.Visible = True
-        Me.colFilename.VisibleIndex = 11
+        Me.colFilename.VisibleIndex = 10
         '
         'colTags
         '
         Me.colTags.FieldName = "Tags"
         Me.colTags.Name = "colTags"
-        Me.colTags.OptionsColumn.ReadOnly = True
         Me.colTags.Visible = True
-        Me.colTags.VisibleIndex = 12
+        Me.colTags.VisibleIndex = 11
         '
-        'AxWMP
+        'FolderBrowserDialog1
         '
-        Me.AxWMP.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.AxWMP.Enabled = True
-        Me.AxWMP.Location = New System.Drawing.Point(0, 402)
-        Me.AxWMP.Name = "AxWMP"
-        Me.AxWMP.OcxState = CType(resources.GetObject("AxWMP.OcxState"), System.Windows.Forms.AxHost.State)
-        Me.AxWMP.Size = New System.Drawing.Size(648, 33)
-        Me.AxWMP.TabIndex = 1
+        Me.FolderBrowserDialog1.Description = "Please provide the folder path of the sounds library:"
+        Me.FolderBrowserDialog1.ShowNewFolderButton = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.btnLibPath)
+        Me.Panel1.Controls.Add(Me.wmp)
+        Me.Panel1.Controls.Add(Me.btnPlaySound)
+        Me.Panel1.Location = New System.Drawing.Point(0, 393)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(648, 39)
+        Me.Panel1.TabIndex = 1
+        '
+        'btnLibPath
+        '
+        Me.btnLibPath.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnLibPath.Location = New System.Drawing.Point(488, 13)
+        Me.btnLibPath.Name = "btnLibPath"
+        Me.btnLibPath.Size = New System.Drawing.Size(75, 23)
+        Me.btnLibPath.TabIndex = 2
+        Me.btnLibPath.Text = "Set lib path"
+        Me.btnLibPath.UseVisualStyleBackColor = True
+        '
+        'wmp
+        '
+        Me.wmp.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.wmp.Enabled = True
+        Me.wmp.Location = New System.Drawing.Point(3, 4)
+        Me.wmp.Name = "wmp"
+        Me.wmp.OcxState = CType(resources.GetObject("wmp.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.wmp.Size = New System.Drawing.Size(220, 33)
+        Me.wmp.TabIndex = 1
+        '
+        'btnPlaySound
+        '
+        Me.btnPlaySound.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnPlaySound.Location = New System.Drawing.Point(569, 13)
+        Me.btnPlaySound.Name = "btnPlaySound"
+        Me.btnPlaySound.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlaySound.TabIndex = 0
+        Me.btnPlaySound.Text = "Play sound"
+        Me.btnPlaySound.UseVisualStyleBackColor = True
+        '
+        'FilesTableAdapter
+        '
+        Me.FilesTableAdapter.ClearBeforeFill = True
+        '
+        'FilesJoinedBindingSource
+        '
+        Me.FilesJoinedBindingSource.DataMember = "FilesJoined"
+        Me.FilesJoinedBindingSource.DataSource = Me.SoundsDataSet
+        '
+        'FilesJoinedTableAdapter
+        '
+        Me.FilesJoinedTableAdapter.ClearBeforeFill = True
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(648, 435)
-        Me.Controls.Add(Me.AxWMP)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.SoundsGrid)
         Me.Name = "Main"
         Me.Text = "Sound Management"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-        CType(Me.SoundsDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.FilesDataTableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.SoundsDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.SoundsGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FilesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SoundsDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.AxWMP, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.wmp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FilesJoinedBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents SoundsDataSet As SoundsManagement.SoundsDataSet
-    Friend WithEvents FilesDataTableBindingSource As System.Windows.Forms.BindingSource
-    Friend WithEvents FilesDataTableTableAdapter As SoundsManagement.SoundsDataSetTableAdapters.FilesDataTableTableAdapter
-    Friend WithEvents SoundsDataSetBindingSource As System.Windows.Forms.BindingSource
     Friend WithEvents SoundsGrid As DevExpress.XtraGrid.GridControl
     Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
     Friend WithEvents colCreator As DevExpress.XtraGrid.Columns.GridColumn
@@ -238,10 +295,19 @@ Partial Class Main
     Friend WithEvents colCategory As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents colSubCategory As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents colDescription As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents colTime As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents colRating As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents colFilename As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents colTags As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents AxWMP As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnPlaySound As System.Windows.Forms.Button
+    Friend WithEvents wmp As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents colTime As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents btnLibPath As System.Windows.Forms.Button
+    Friend WithEvents SoundsDataSet As SoundsManagement.SoundsDataSet
+    Friend WithEvents FilesBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents FilesTableAdapter As SoundsManagement.SoundsDataSetTableAdapters.FilesTableAdapter
+    Friend WithEvents FilesJoinedBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents FilesJoinedTableAdapter As SoundsManagement.SoundsDataSetTableAdapters.FilesJoinedTableAdapter
 
 End Class
--- SoundsManagement/SoundsManagement/Main.vb
+++ SoundsManagement/SoundsManagement/Main.vb
@@ -1,26 +1,58 @@
-﻿Public Class Main
+﻿Imports DevExpress.XtraGrid.Views.Grid
 
+Public Class Main
     Private _FilesPath As String = ""
-    Public ReadOnly Property FilesPath As String
+    Public ReadOnly Property FilesPath(Optional ByVal RetrieveFromDB As Boolean = True) As String
         Get
             If _FilesPath = "" Then
-                Dim frm As New FilesPathDialog
-                If frm.ShowDialog = vbOK Then _FilesPath = frm.txtPath.Text
+                Dim que As New SoundsDataSetTableAdapters.QueriesTableAdapter
+                Dim pt As New SoundsDataSetTableAdapters.ParametersTableAdapter
+                If RetrieveFromDB Then _FilesPath = que.LibraryPath()
+                If _FilesPath <> "" AndAlso My.Computer.FileSystem.DirectoryExists(_FilesPath) Then Return _FilesPath
+                If FolderBrowserDialog1.ShowDialog = vbOK Then
+                    _FilesPath = FolderBrowserDialog1.SelectedPath
+                    Try
+                        pt.Update(_FilesPath, "SoundsLibraryPath", "")
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
+                    End Try
+                    SoundsDataSet.AcceptChanges()
+                End If
             End If
             Return _FilesPath
         End Get
     End Property
 
-
     Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Me.FilesDataTableTableAdapter.Fill(Me.SoundsDataSet._FilesDataTable)
+        Me.FilesJoinedTableAdapter.Fill(Me.SoundsDataSet.FilesJoined)
+        wmp.settings.autoStart = True
         Dim s As String = FilesPath
     End Sub
 
+    Private Sub btnPlaySound_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPlaySound.Click
+        PlaySound()
+    End Sub
+
     Private Sub SoundsGrid_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles SoundsGrid.DoubleClick
-        Dim row As SoundsManagement.SoundsDataSet.FilesDataTableRow
-        'row = SoundsGrid.FocusedView.GetRow
-        'AxWMP.URL = SoundsGrid.fo
-        Dim s As String = ""
+        PlaySound()
+    End Sub
+
+    Private Sub PlaySound()
+        Dim row As SoundsDataSet.FilesJoinedRow 'SoundsManagement.SoundsDataSet.FilesDataTableRow
+        Dim CurrentFile As String
+        Dim tr As System.Data.DataRowView
+        tr = SoundsGrid.FocusedView.GetRow(CType(SoundsGrid.FocusedView, DevExpress.XtraGrid.Views.Grid.GridView).FocusedRowHandle)
+        row = tr.Row
+        CurrentFile = FilesPath & "\" & (row.Creator & " - " & row.Library & "\" & row.CD & "\" & row.Filename & ".mp3").Replace("/", "\")
+        If My.Computer.FileSystem.FileExists(CurrentFile) Then
+            wmp.URL = CurrentFile
+        Else
+            MessageBox.Show("File: " & CurrentFile & " does not exist")
+        End If
+    End Sub
+
+    Private Sub btnLibPath_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLibPath.Click
+        _FilesPath = ""
+        Dim s As String = FilesPath(False)
     End Sub
 End Class
